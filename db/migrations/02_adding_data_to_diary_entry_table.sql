d67e929e2b8a202dff82e8cd2a4b53b246f52700
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-21 09:58:30 +0100

    extracts entries to database

---
+++ db/migrations/02_adding_data_to_diary_entry_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO diary_entries (title) VALUES('Haircut');
+INSERT INTO diary_entries (title) VALUES('Cool bike ride);
--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,9 +1,10 @@
+require 'pg'
+
 class DiaryEntry
   def self.all
-    [
-      "Haircut",
-      "Cool bike ride"
-    ]
+    connection = PG.connect(dbname: 'daily_diary')
+    result = connection.exec("SELECT * FROM diary_entries;")
+    result.map { |entry| entry['title'] }
 
   end
 end
